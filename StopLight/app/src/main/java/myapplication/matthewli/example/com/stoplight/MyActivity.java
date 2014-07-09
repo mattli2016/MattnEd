833fa06de94c54cd4762cd4dfd3a8722834361ae
Edward Gong <edwardgong12@gmail.com>
2014-07-09 01:16:11 -0700

    Add StopLight app. Should be working

---
+++ StopLight/app/src/main/java/myapplication/matthewli/example/com/stoplight/MyActivity.java
@@ -0,0 +1,48 @@
+package myapplication.matthewli.example.com.stoplight;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        findViewById(R.id.my_button).setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.my_button) {
+            TextView tv = (TextView) findViewById(R.id.displayed_text);
+            tv.setText("Clicked!");
+        }
+    }
+
+}
